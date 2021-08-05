a64160de01f387a7e4f214cd0ee4a38f6b60b00d
Sarthak Ladhwe <sarthak.ladhwe156@gmail.com>
2021-08-06 00:46:00 +0530

    Added loading meme

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 window.addEventListener('load', () => {
+    const preloader = document.querySelector('.preloader');
+    const location = document.querySelector('.location');
+    const temperature = document.querySelector('.temperature');
+
     let long;
     let lat;
 
@@ -9,23 +13,19 @@ window.addEventListener('load', () => {
     let degreeSection = document.querySelector('.degree-section');
     let temperatureSpan = document.querySelector('.degree-section span');
 
-
     if(navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(position => {
-            console.log(position);
             long = position.coords.longitude;
             lat = position.coords.latitude;
 
-            const proxy = 'https://cors-anywhere.herokuapp.com/'
             //const api = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&units=metric&appid=3ed574e586d49fbadfba40a6860f20cf`;
-            const api = `${proxy}api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&units=metric&appid=3ed574e586d49fbadfba40a6860f20cf`;
+            const api = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&units=metric&appid=3ed574e586d49fbadfba40a6860f20cf`;
 
             fetch(api)
             .then(response => {
                 return response.json()
             })  
             .then(data => {
-                console.log(data);
                 const timezone = data.name;
                 const {temp} = data.main;
                 const {description, icon} = data.weather[0];
@@ -33,11 +33,17 @@ window.addEventListener('load', () => {
                 //Set DOM elements from the AP
                 temperatureDegree.textContent = temp.toFixed(1);
                 locationTimezone.textContent = timezone;
-                temperatureDescription.textContent = description;
+                temperatureDescription.textContent = description.charAt(0).toUpperCase() + description.slice(1);;
 
                 //http://openweathermap.org/img/wn/04d@2x.png
                 imageIcon.src = "http://openweathermap.org/img/wn/" + icon + "@4x.png";
 
+                setTimeout(() => {
+                    preloader.classList.add('preloader-finish');
+                    location.style.display = "initial";
+                    temperature.style.display = "initial";
+                },(3*1000));
+                
                 //Celcius to F
                 let fahrenheit = temp * 9 / 5 + 32;
 
@@ -53,6 +59,4 @@ window.addEventListener('load', () => {
             })   
         });
     }
-
-    
 });
